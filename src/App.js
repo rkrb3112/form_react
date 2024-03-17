eb978beec5561745d3ccb180dbcfb9aefbb74b91
rkrb3112 <rkrb3112@gmail.com>
2024-03-17 09:47:17 +0530

    file changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Home } from './pages/Home';
-import { Menu } from './pages/Menu';
-import { Contact } from './pages/Contact';
-import { Navbar } from './Navbar';
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import { Form } from './components/Form';
 
 function App() {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false
-      }
-    }
-  });
   return ( 
     <div className='App'>
-      <QueryClientProvider client={client}>
-          <Router>
-            <Navbar />
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/menu' element={<Menu />} />
-              <Route path='/contact' element={<Contact />} />
-              <Route path='*' element={<h1>Does not exist</h1>} />
-            </Routes>
-          </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 };
